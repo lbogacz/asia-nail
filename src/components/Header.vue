57969a2907f689bca17957512307bf872d12444f
lbogacz <lukas.bogacz@gmail.com>
2019-03-10 00:57:29 +0000

    add toolbar

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="header">
+<div class="header-mobile">
+    <img src="../assets/pexels-photo-1889347.jpeg" alt="black women">
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    name:"Header",
+}
+</script>
+
+<style lang="scss" scoped>
+.header-mobile{
+  img{
+      height: 100px;
+      widows: 100px;
+  }
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <v-toolbar dark>
+      <v-toolbar-side-icon @click="drawer=!drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <v-navigation-drawer dark v-model="drawer" app>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon>{{links.icon}}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "", text: "Strona Domowa", route: "/" },
+        { icon: "", text: "Galeria", route: "/gallery" },
+        { icon: "", text: "Usługi", route: "/service" },
+        { icon: "", text: "O mnie", route: "/about" }
+      ]
+    };
+  }
+};
+</script>
