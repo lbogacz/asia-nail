39a6b542ebf0516fb998c201abac8a6f7112509d
lbogacz <lukas.bogacz@gmail.com>
2019-03-12 23:26:31 +0000

    upgrade menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <v-menu offset-y dark >
+    <v-menu offset-y dark bottom origin="center center" transition="scale-transition">
       <template v-slot:activator="{ on }">
         <v-toolbar dark flat>
           <v-toolbar-side-icon v-on="on"></v-toolbar-side-icon>
@@ -8,7 +8,13 @@
       </template>
 
       <v-list>
-        <v-list-tile v-for="link in links" :key="link.text" @click="menu = !menu" route :to="link.route">
+        <v-list-tile
+          v-for="link in links"
+          :key="link.text"
+          @click="menu = !menu"
+          route
+          :to="link.route"
+        >
           <v-list-tile-title>{{ link.text }}</v-list-tile-title>
         </v-list-tile>
       </v-list>
@@ -32,3 +38,13 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.v-menu__content {
+  width: 100%;
+  max-width: 100%;
+}
+.v-list__tile__title {
+  text-align: center;
+}
+</style>
