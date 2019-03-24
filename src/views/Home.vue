df6945a92e73281d0d70d942db9a5e0280698e0a
lbogacz <lukas.bogacz@gmail.com>
2019-03-24 23:29:45 +0000

    add few things

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,21 @@
 <template>
   <div class="app-home">
-    <h2>Welcome message</h2>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, tempora recusandae reiciendis sequi enim nihil sapiente quasi laboriosam nam doloremque consequuntur, dolorem sit fugiat voluptatibus ut eaque, impedit sed vero.</p>
+
+    <div class="app-home-header">
+      <h2>Welcome message</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, tempora recusandae reiciendis sequi enim nihil sapiente quasi laboriosam nam doloremque consequuntur, dolorem sit fugiat voluptatibus ut eaque, impedit sed vero.</p>
+      <router-link :to='{name:"service"}'>
+        <v-btn>Wiecej...</v-btn>
+      </router-link>
+    </div>
+
+    <div class="app-home-content">
+    <h2>Manicure</h2>
+    <img src="../assets/pexels-photo-361754.jpeg" alt="manicure">
     <router-link :to='{name:"service"}'>
-      <v-btn>More...</v-btn>
-    </router-link>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A ducimus accusantium ea quo delectus labore itaque perferendis quia, possimus consectetur soluta nobis? Libero error aut animi, ad assumenda dolorum eius.</p>
+        <v-btn>Wiecej...</v-btn>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -24,14 +23,18 @@
 export default {
   name: "Home",
 
-    data() {
-      return {};
-    }  }
-
+  data() {
+    return {};
+  }
+};
 </script>
  <style lang="scss" scoped>
 .app-home {
   text-align: center;
+}
+
+.app-home-header {
+padding:30px;
   h2 {
     color: #fff;
     font-size: 24px;
@@ -42,4 +45,25 @@ export default {
     font-size: 14px;
   }
 }
+
+ a {
+    text-decoration: none;
+  }
+
+  .app-home-content{
+    padding:30px;
+
+    color: #fff;
+    h2{
+    padding: 10px 2px;
+    }
+    img{
+    height:200px;
+    width:100%;
+    object-fit: cover;
+    }
+    button{
+      margin-top:15px;
+    }
+  }
 </style>
