75e6f36042ace77ac31ea53f2b56da5b7e375771
lbogacz <lukas.bogacz@gmail.com>
2019-03-27 23:21:24 +0000

    add rest to the homePage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="app-home">
-
     <div class="app-home-header">
       <h2>Welcome message</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, tempora recusandae reiciendis sequi enim nihil sapiente quasi laboriosam nam doloremque consequuntur, dolorem sit fugiat voluptatibus ut eaque, impedit sed vero.</p>
@@ -10,11 +9,31 @@
     </div>
 
     <div class="app-home-content">
-    <h2>Manicure</h2>
-    <img src="../assets/pexels-photo-361754.jpeg" alt="manicure">
-    <router-link :to='{name:"service"}'>
-        <v-btn>Wiecej...</v-btn>
-      </router-link>
+
+      <div class="app-home-content-manicure">
+        <h2>Manicure</h2>
+        <img src="../assets/pexels-photo-361754.jpeg" alt="manicure">
+        <router-link :to='{name:"service"}'>
+          <v-btn color="purple darken-1">Wiecej...</v-btn>
+        </router-link>
+      </div>
+
+      <div class="app-home-content-manicureArt">
+        <h2>Manicure Art</h2>
+        <img src="../assets/beauty-fingernails-hearts-37553.jpg" alt="manicure art">
+        <router-link :to='{name:"service"}'>
+          <v-btn color="pink accent-1">Wiecej...</v-btn>
+        </router-link>
+      </div>
+
+      <div class="app-home-content-repair">
+        <h2>Naprawa</h2>
+        <img src="../assets/pexels-photo-887352.jpeg" alt="repair nail">
+        <router-link :to='{name:"service"}'>
+          <v-btn color="#B6254A">Wiecej...</v-btn>
+        </router-link>
+      </div>
+
     </div>
   </div>
 </template>
@@ -29,16 +48,22 @@ export default {
 };
 </script>
  <style lang="scss" scoped>
+
 .app-home {
   text-align: center;
+  color: #fff;
+   padding: 30px;
 }
 
+h2{
+ padding: 10px 2px;
+ }
+
+
 .app-home-header {
-padding:30px;
+  margin-bottom:30px;
   h2 {
-    color: #fff;
     font-size: 24px;
-    padding: 10px 2px;
   }
   p {
     color: #959595;
@@ -46,24 +71,42 @@ padding:30px;
   }
 }
 
- a {
-    text-decoration: none;
-  }
+a {
+  text-decoration: none;
+}
 
-  .app-home-content{
-    padding:30px;
-
-    color: #fff;
-    h2{
-    padding: 10px 2px;
-    }
-    img{
-    height:200px;
-    width:100%;
+.app-home-content {
+  img {
+    height: 200px;
+    width: 100%;
     object-fit: cover;
-    }
-    button{
-      margin-top:15px;
+  }
+  button {
+    margin-top: 15px;
+  }
+}
+
+.app-home-content-manicure,
+.app-home-content-manicureArt,
+.app-home-content-repair{
+  margin-bottom:30px;
+}
+
+.app-home-content-manicure{
+  h2{
+    color:#8E24AA;
+  }
+}
+
+.app-home-content-manicureArt{
+  h2{
+    color:#FF80AB;
+  }
+}
+  .app-home-content-repair{
+    h2{
+    color:#B6254A;
     }
   }
+
 </style>
